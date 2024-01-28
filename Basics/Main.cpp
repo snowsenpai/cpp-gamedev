1a50d7fb1062788aa487f51d8e5b2dd69df209ca
Umoh Samuel <samuelwisdom360@gmail.com>
2024-01-28 20:26:22 +0100

    Attempt to load texture from file

--- Basics/Main.cpp
+++ Basics/Main.cpp
@@ -6,6 +6,24 @@ int main()
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Magenta);
 
+	// relative and absolute file paths fails
+	sf::Texture playerWalk1;
+	if (!playerWalk1.loadFromFile("assets/art/playerGery_walk1.png"))
+		return EXIT_FAILURE;
+	playerWalk1.setSmooth(true);
+	playerWalk1.setRepeated(true);
+
+	sf::Texture playerWalk2;
+	if (!playerWalk2.loadFromFile("assets/art/playerGery_walk2.png"))
+		return EXIT_FAILURE;
+	playerWalk2.setSmooth(true);
+	playerWalk2.setRepeated(true);
+
+	sf::Sprite playerWalk;
+	playerWalk.setTexture(playerWalk1);
+	playerWalk.setTexture(playerWalk2);
+
+	playerWalk.setPosition(sf::Vector2f(10.f, 50.f));
 	while (window.isOpen())
 	{
 		sf::Event event;

f6727a947aba73c0eacf227cf5bebbac884464f1
Umoh Samuel <samuelwisdom360@gmail.com>
2024-01-29 10:11:12 +0100

    Load texture from file works...

--- Basics/Main.cpp
+++ Basics/Main.cpp
@@ -1,29 +1,26 @@
 #include <SFML/Graphics.hpp>
 
-int main()
+int main(int argc, char * argv[])
 {
-	sf::RenderWindow window(sf::VideoMode(720, 360), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Magenta);
+	sf::RenderWindow window(sf::VideoMode(720, 360), "SFML Basics!");
 
-	// relative and absolute file paths fails
 	sf::Texture playerWalk1;
-	if (!playerWalk1.loadFromFile("assets/art/playerGery_walk1.png"))
+	if (!playerWalk1.loadFromFile("assets/art/playerGrey_walk1.png"))
 		return EXIT_FAILURE;
 	playerWalk1.setSmooth(true);
-	playerWalk1.setRepeated(true);
-
+	
 	sf::Texture playerWalk2;
-	if (!playerWalk2.loadFromFile("assets/art/playerGery_walk2.png"))
+	if (!playerWalk2.loadFromFile("assets/art/playerGrey_walk2.png"))
 		return EXIT_FAILURE;
 	playerWalk2.setSmooth(true);
-	playerWalk2.setRepeated(true);
 
 	sf::Sprite playerWalk;
+	sf::Sprite playerWalking;
 	playerWalk.setTexture(playerWalk1);
-	playerWalk.setTexture(playerWalk2);
+	playerWalking.setTexture(playerWalk2);
 
 	playerWalk.setPosition(sf::Vector2f(10.f, 50.f));
+	playerWalking.setPosition(sf::Vector2f(10.f, 50.f));
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -33,7 +30,8 @@ int main()
 				window.close();
 		}
 		window.clear();
-		window.draw(shape);
+		window.draw(playerWalk);
+		window.draw(playerWalking);
 		window.display();
 	}
 

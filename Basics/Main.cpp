a681cf5af458b872ade954626e21e9a08ca6552f
Umoh Samuel <samuelwisdom360@gmail.com>
2024-02-04 16:06:23 +0100

    Work with SFML Audio module

--- Basics/Main.cpp
+++ Basics/Main.cpp
@@ -1,10 +1,14 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <iostream>
 
 int main(int argc, char * argv[])
 {
 	sf::RenderWindow window(sf::VideoMode(720, 360), "SFML Basics!");
 	window.setFramerateLimit(60);
+	window.setKeyRepeatEnabled(true);
+	const float MAX_VOL = 100.f;
+	const float MIN_VOL = 0.f;
 
 	sf::Texture playerWalk1;
 	if (!playerWalk1.loadFromFile("assets/art/playerGrey_walk1.png"))
@@ -64,13 +68,30 @@ int main(int argc, char * argv[])
 	sf::FloatRect squareBoundingBox = square.getGlobalBounds();
 
 	if (squareBoundingBox.intersects(hello.getGlobalBounds()))
-		std::cout << "squre is touching hello text" << std::endl;
+		std::cout << "sqaure is touching hello text" << std::endl;
 	else
-		std::cout << "squre is not touching hello text" << std::endl;
+		std::cout << "sqaure is not touching hello text" << std::endl;
 
 	sf::CircleShape octagon(30.f, 8);
 	octagon.setFillColor(sf::Color(250, 200, 150));
 	octagon.setPosition(sf::Vector2f(100.f, 30.f));
+
+	sf::SoundBuffer gameoverBuffer;
+	if (!gameoverBuffer.loadFromFile("assets/audio/gameover.wav"))
+		return -1;
+
+	sf::Sound gameover;
+	gameover.setBuffer(gameoverBuffer);
+
+	sf::Music forestLoop;
+	if (!forestLoop.openFromFile("assets/audio/House In a Forest Loop.ogg"))
+		return -1;
+	forestLoop.setVolume(50.f);
+	forestLoop.setLoop(true);
+	forestLoop.play();
+	float forestLoopVol = forestLoop.getVolume();
+	//prevent vol increase and decrease if getVolume is at 100 and 0 respectively
+	//bool changeVol = forestLoopVol > MIN_VOL && forestLoopVol < MAX_VOL;
 
 	while (window.isOpen())
 	{
@@ -80,6 +101,41 @@ int main(int argc, char * argv[])
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::F))
+			gameover.play();
+
+		sf::Sound::Status forestLoopStatus = forestLoop.getStatus();
+		switch (forestLoopStatus)
+		{
+		case sf::SoundSource::Stopped:
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::Space))
+				forestLoop.play();
+			break;
+		case sf::SoundSource::Paused:
+//using space key to pause and play isn't smooooth, key is triggered multiple times, same for gameover sound
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::C))
+				forestLoop.play();
+			break;
+		case sf::SoundSource::Playing:
+			//pause or stop the music
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::V))
+				forestLoop.pause();
+			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::Backspace))
+				forestLoop.stop();
+
+			//increase/reduce volume
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::Up))
+				// increment and decrement values are not by 1 for floats but btw 6,7,8
+				forestLoop.setVolume(forestLoopVol += 1);
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::Down))
+				forestLoop.setVolume(forestLoopVol -= 1);
+			std::cout << "volume " << forestLoop.getVolume() << std::endl;
+			break;
+		default:
+			break;
+		}
+
 		window.clear();
 		window.draw(hello);
 		window.draw(circle);

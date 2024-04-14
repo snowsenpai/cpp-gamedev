e6174fa1a5a31c0ea8b4f5a7ba503cc664112a72
Umoh Samuel <samuelwisdom360@gmail.com>
2024-04-14 23:17:34 +0100

    Setup Geom:
    add required files to project
    
    add class declarations for Vec2 and basic Components and definitions for Vec2 member functions

---
+++ Geom/Components.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "Vec2.h"
+#include <SFML/Graphics.hpp>
+
+class CTransform
+{
+public:
+	Vec2 pos = { 0.0, 0.0 };
+	Vec2 velocity = { 0.0, 0.0 };
+	float angle = 0;
+
+	CTransform(const Vec2 & p, const Vec2 & v, float a)
+		: pos(p), velocity(v), angle(a) {}
+};
+
+class CShape
+{
+public:
+	sf::CircleShape circle;
+
+	CShape(float radius, int points, const sf::Color& fill, const sf::Color& outline, float thickness)
+		: circle(radius, points)
+	{
+		circle.setFillColor(fill);
+		circle.setOutlineColor(outline);
+		circle.setOutlineThickness(thickness);
+		circle.setOrigin(radius, radius);
+	}
+};
+
+class CCollision
+{
+public:
+	float radius = 0;
+	CCollision(float r)
+		: radius(r) {}
+};
+
+class CScore
+{
+public:
+	int score = 0;
+	CScore(int s)
+		: score(s) {}
+};
+
+class CLifeSpan
+{
+public:
+	// amount of life span remaining in the entity
+	int remaining = 0;
+	// the total inirial amount of lifespan
+	int total = 0;
+	ClifeSspan(int total)
+		: remaining(total), total(total) {}
+};
+
+class CInput
+{
+public:
+	bool up = false;
+	bool down = false;
+	bool left = false;
+	bool right = false;
+
+	CInput() {}
+};
---
+++ Geom/Vec2.cpp
@@ -0,0 +1,65 @@
+#include "Vec2.h"
+
+Vec2::Vec2() {}
+
+Vec2::Vec2(float xin, float yin) : x(xin), y(yin) {}
+
+bool Vec2::operator == (const Vec2& rhs) const
+{
+	return (x == rhs.x && y == rhs.y);
+}
+
+bool Vec2::operator != (const Vec2& rhs) const
+{
+	return (x != rhs.x && y != rhs.y);
+}
+
+Vec2 Vec2::operator + (const Vec2& rhs) const
+{
+	return Vec2(x + rhs.x, y + rhs.y);
+}
+
+Vec2 Vec2::operator - (const Vec2& rhs) const
+{
+	return Vec2(x - rhs.x, y - rhs.y);
+}
+
+Vec2 Vec2::operator * (const float val) const
+{
+	return Vec2(x * val, y * val);
+}
+
+Vec2 Vec2::operator / (const float val) const
+{
+	return Vec2(x / val, y / val);
+}
+
+void Vec2::operator += (const Vec2& rhs)
+{
+	x += rhs.x;
+	y += rhs.y;
+}
+
+void Vec2::operator -= (const Vec2& rhs)
+{
+	x -= rhs.x;
+	y -= rhs.y;
+}
+
+void Vec2::operator *= (const float val)
+{
+	x *= val;
+	y *= val;
+}
+
+void Vec2::operator /= (const float val)
+{
+	x /= val;
+	y /= val;
+}
+
+float Vec2::dist(const Vec2& rhs) const
+{
+	// TODO calc distance between origin(this *) and destination (rhs)
+	return 1.f;
+}
